b502818cbbb4e4870eb459c242b7248228a17f29
hanbinchoi <wkghskak@naver.com>
2021-11-07 20:35:22 +0900

    input-form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -9,6 +10,14 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링" done={true} />
+      <TodoItem text="프로젝트 버리기" done={false} />
+      <TodoItem text="롤하기" done={true} />
+      <TodoItem text="왤케빨라" done={false} />
+    </TodoListBlock>
+  );
 }
 export default TodoList;
